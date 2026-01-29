2f0887e6ae3ccb2c0656ea9a99bad422244d6ef9
Alexander B. Prescott <alexander.prescott@uvm.edu>
2026-01-29 17:47:59 -0500

    small edit

--- preptevainputs.py
+++ preptevainputs.py
@@ -2,7 +2,7 @@ from classes import *
 
 
 output_dir = '/root/repositories/sedregfromedz/data/teva/'
-hm_dir = '/root/SRCclusters/results/EDZcode/5Nov2025/' # 8Jul2025/'
+hm_dir = '/root/SRCclusters/results/EDZcode/10Dec2025/'
 geomorph_path = hm_dir + 'network/reach_data.csv'
 edz_path = hm_dir + 'analysis/data.csv'
 reaches = ReachData(geomorphic_data_path=geomorph_path, edz_data_path=edz_path)
